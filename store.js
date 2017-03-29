b6b2b66b2a323caf744f2d31263fa91e3d65cdac
Pankaj Sabharwal <pankaj.sabharwal@mercer.com>
2017-03-29 15:06:15 +0530

    Revert "UI look and feel changes"
    This reverts commit 3d70e1262aeece8729498cc054e2904702393203.

--- store.js
+++ store.js
@@ -2,26 +2,15 @@ var Promise = require('bluebird');
 var userdata = require('./userdata.json');
 
 module.exports = {
-    validateuser: function (userid, password) {
-        var _user;
-        for (var i = 0, l = userdata.users.length; i < l; i++) {
-            var obj = userdata.users[i];
-            if (obj.userid === userid && obj.password === password) {
-                _user = obj;
-                break;
+    validateuser: function(userid, password){
+            var _user;
+            for (var i = 0, l = userdata.users.length; i < l; i++){
+                var obj = userdata.users[i];
+                if (obj.userid === userid && obj.password === password) {
+                    _user = obj;
+                    break;
+                }
             }
-        }
-        return _user;
-    },
-    validateuseronly: function (userid) {
-        var _user;
-
-        for (var i = 0, l = userdata.users.length; i < l; i++) {
-            var obj = userdata.users[i];
-            if (obj.userid === userid) {
-                return obj;
-            }
-        }
-        return _user;
+            return _user;
     }
 };
